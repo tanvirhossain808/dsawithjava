5ccb8581ed66b2b413f0532dec443990e57ce29f
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-04-16 00:04:34 +0600

    add bitmanuplation first part

---
+++ Bitmanipulation/BitwiseAnd.java
@@ -0,0 +1,14 @@
+package Bitmanipulation;
+
+public class BitwiseAnd {
+    public static void main(String[] args) {
+        // System.out.print(5 & 6);
+        // System.out.println(5 | 6);
+        // System.out.println(5 ^ 6);
+        // not/one's complement=> System.out.println(~5);
+        // System.out.println(~0);
+        // Binary Left Shift System.out.println(5 << 2);
+        // Binary Right shift System.out.println(6 >> 1);
+
+    }
+}
