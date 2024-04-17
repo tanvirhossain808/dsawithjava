3b0cb8dd3336b0c6254cb8e77ef326386d277aca
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-04-18 03:39:26 +0600

    add polyMethodOver riding

---
+++ Opps/PolyMethodOverRiding.java
@@ -0,0 +1,19 @@
+
+public class PolyMethodOverRiding {
+    public static void main(String[] args) {
+        Tiger tiger = new Tiger();
+        tiger.eat();
+    }
+}
+
+class Animal {
+    void eat() {
+        System.out.println("anythin");
+    }
+}
+
+class Tiger {
+    void eat() {
+        System.out.println("eat only beef");
+    }
+}
