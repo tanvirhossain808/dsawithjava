532b418fccaadb1cab2094491f2d377be6e93797
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-04-21 03:37:50 +0600

    add piar of frinds

---
+++ Recursion/PiarOfFriend.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class PiarOfFriend {
+    public static int pairFrind(int friend) {
+
+        if (friend == 1 || friend == 2) {
+            return friend;
+        }
+
+        return pairFrind(friend - 1) + (pairFrind(friend - 1) * pairFrind(friend - 2));
+
+    }
+
+    public static void main(String[] args) {
+        int frind = 3;
+        System.out.println(pairFrind(3));
+    }
+}
