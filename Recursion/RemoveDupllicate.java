4409b32e520f2ec898e3e8a86154334187a721dc
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-04-21 02:51:39 +0600

    add remove duplicate using recursion

---
+++ Recursion/RemoveDupllicate.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+public class RemoveDupllicate {
+    public static void printNewString(String character, int idx, StringBuilder newStr, boolean map[]) {
+        if (idx == character.length()) {
+            System.out.println(newStr);
+            return;
+        }
+        char currentChar = character.charAt(idx);
+        if (map[currentChar - 'a'] == true) {
+            printNewString(character, idx + 1, newStr, map);
+        } else {
+            map[currentChar - 'a'] = true;
+            printNewString(character, idx + 1, newStr.append(currentChar), map);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String character = "mangomanog";
+        printNewString(character, 0, new StringBuilder(""), new boolean[26]);
+
+    }
+
+}
